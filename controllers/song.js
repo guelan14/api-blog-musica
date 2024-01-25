99f9f3a7b542c44453803b5911e84fbb8c445b89
Miguel Angel Neumann <miguelangelneumann@gmail.com>
2024-01-25 17:16:35 -0300

    agregado song controllers

--- controllers/song.js
+++ controllers/song.js
@@ -31,4 +31,99 @@ const save = (req, res) => {
     });
 };
 
-module.exports = { prueba, save };
+const list = (req, res) => {
+  let albumId = req.params.albumId;
+  Song.find({ album: albumId })
+    .sort("track")
+    .then((song) => {
+      return res.status(200).send({
+        status: "succes",
+        message: "mensaje desde song",
+      });
+    });
+
+  return res.status(500).send({
+    status: "error",
+    message: "error al guardar cancion",
+  });
+};
+
+const remove = (req, res) => {
+  const songId = req.params.id;
+  Song.findByIdAndRemove(songId)
+    .then((songRemoved) => {
+      return res.status(200).send({
+        status: "succes",
+        message: "mensaje desde song",
+        songRemoved,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).send({
+        status: "error",
+        message: "error al guardar cancion",
+      });
+    });
+};
+
+const upload = (req, res) => {
+  let songId = req.params.id;
+
+  if (!req.file) {
+    return res.status(404).send({
+      status: "error",
+      message: "la peticion no incluye imagen",
+    });
+  }
+  let image = req.file.originalname;
+  const imageSplit = image.split(".");
+  const extension = imageSplit[1];
+
+  if (extension != "mp3" && extension != "ogg") {
+    const filePath = req.file.path;
+    console.log(filePath);
+    const fileDeleted = fs.unlinkSync(filePath);
+
+    return res
+      .status(404)
+      .send({ status: "error", mesage: "la extension no es valida" });
+  }
+
+  Song.findOneAndUpdate(
+    { _id: songId },
+    { image: req.file.filename },
+    { new: true }
+  )
+    .then((songUpdated) => {
+      return res.status(200).send({
+        status: "succes",
+        song: songUpdated,
+        file: req.file,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).send({
+        status: "error",
+        message: "Error en la subida",
+      });
+    });
+};
+
+const audio = (req, res) => {
+  const file = req.params.file;
+  const filePath = "./uploads/songs/" + file;
+
+  console.log(filePath);
+
+  fs.stat(filePath, (error, exists) => {
+    if (error || !exists) {
+      return res
+        .status(404)
+        .send({ status: "error", message: "No existe la imagen" });
+    }
+
+    return res.sendFile(path.resolve(filePath));
+  });
+};
+
+module.exports = { prueba, save, list, remove, upload, audio };
--- routes/song.js
+++ routes/song.js
@@ -4,7 +4,28 @@ const router = express.Router();
 const SongController = require("../controllers/song");
 const check = require("../middlewares/auth");
 
+//multer config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/albums/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "album-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
 router.get("/prueba", SongController.prueba);
 router.post("/save", check.auth, SongController.save);
+router.get("/one/:id", check.auth, SongController.one);
+router.get("/list/:albumId", check.auth, SongController.list);
+router.delete("/remove/:id", check.auth, SongController.remove);
+router.post(
+  "/upload/:id",
+  [check.auth, uploads.single("file0")],
+  SongController.upload
+);
+router.get("/audio/:file", SongController.audio);
 
 module.exports = router;
